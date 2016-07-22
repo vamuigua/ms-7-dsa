f34fa1c815776c07cc15983103ed98c4b62c6b0f
kimaiga <nelsonkimaiga@aol.com>
2016-07-22 08:41:09 +0300

    added maximum minimum problem

---
+++ third_largest/Maximum_minimum.java
@@ -0,0 +1,11 @@
+// Using java solve the problem below:
+
+//Create your own array of integers
+// Find the maximum and minimum of this array that you just created.
+// create a variable to hod the maximum
+// create a variable to hold the minimum
+// take in the array of numbers
+// begin to loop through them
+// put the lowest in its array 
+// put the highest in its array
+// output the highest then output the lowest
