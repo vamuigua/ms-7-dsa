e87f13ef565c6ab5067364657db5d01ce52a3adf
Victor Allen <vamuigua@gmail.com>
2016-07-22 09:33:30 +0300

    added third-largest code

---
+++ third_largest/third-largest/src/largest.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class largest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		strArr("hello","world","before");
+		}
+	public static void strArr(String , String string2, String string3) {
+		// TODO Auto-generated method stub
+		
+	}
+	public static String[] strArr(String x, String y, String z){
+		String Arr [] = {x,y,z};
+		Arrays.sort(Arr);
+		System.out.println(Arrays.toString(Arr));
+		return Arr;
+	}
+}
