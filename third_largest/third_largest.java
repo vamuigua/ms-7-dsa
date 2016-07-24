3edf94b5bd4fa4046730907476216eba7a43b39b
Harrisonkamau <kamauharrison87@gmail.com>
2016-07-24 08:35:38 +0300

    attach solution to the algorithm

--- third_largest/third_largest.java
+++ third_largest/third_largest.java
@@ -4,3 +4,31 @@
 // your output should be "world" because "before" is 6 letters long, and "hello" and "world" are both 5, but the output should be world because it appeared as the last 5 letter word in the array. If strArr was ["hello", "world", "after", "all"] the output should be after because the first three words are all 5 letters long, so return the last one. The array will have at least three strings and each string will only contain letters.
 
 // use the main method only for calling the function where your logic lies. :-)
+
+public class ThirdSort {
+
+    /**
+     * @param args the command line arguments
+     */
+    public String ThirdGreatest(String[]strArr){
+        String sortedString = "";
+        boolean finalSort =  true;
+        while(finalSort){
+            for(int i = 0; i < strArr.length-1; i++){
+                if(strArr[i].length() < strArr[i+1].length()){
+                    sortedString = strArr[i];
+                    strArr[i] = strArr[i+1];
+                    strArr[i+1]= sortedString;
+                    finalSort = false;
+                };
+            }
+        }
+        return strArr[2];
+    }
+    public static void main(String[] args) {
+        String[] myArr={"hello", "world", "before", "all"};
+        System.out.println(ThirdGreatest(myArr));
+    }
+    
+}
+
