a20d5137c9c9be7306075534c983d49e33a12cdb
kimaiga <nelsonkimaiga@aol.com>
2016-07-25 08:46:40 +0300

    problem added: Next Highest

---
+++ next_higest/nextHighest.java
@@ -0,0 +1,2 @@
+// Given a number, find the next higher number using only the digits in the given number. 
+//For example if the given number is 1508, the next higher number with same digits is 8510.
