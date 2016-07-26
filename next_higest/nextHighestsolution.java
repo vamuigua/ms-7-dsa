589845331973a13f74eca254bcafdea6a4f45fe2
Victor Allen <vamuigua@gmail.com>
2016-07-26 06:06:15 +0300

    added pseudo code soln

---
+++ next_higest/nextHighestsolution.java
@@ -0,0 +1,7 @@
+Pseudo code:
+//select a random number say 34722641
+//split the number into two e.g '34722' '641'
+//select the last digit in the first sequence 3472(2)
+//find the smallest digit in the second sequence that is larger than it i.e 6(4)1
+//Swap them,therefore the number is 3472(4) 6(2)1
+//sort the second sequence to increasing number: '34724' '126'
